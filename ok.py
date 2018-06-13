12c00e3a19c282e81d9aa3b9399a8fd4a1a11126
bibhu <prasadpal107@gmail.com>
2018-06-13 16:16:05 +0530

    Create ok.py

---
+++ ok.py
@@ -0,0 +1,6 @@
+      with tf.device('/cpu:0'), tf.name_scope("embedding"):
+            self.W = tf.Variable(
+                tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
+                name="W")
+            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.inputTensor)
+            self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
